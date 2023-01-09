1108ca7fa063c7667b81cc30ffaf412ee67a8a2e
Roodolph <roodysenecal@gmail.com>
2023-01-08 23:28:31 -0500

    Hero Section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './hero.css'
+import meditating from '../../assets/meditating.png'
 const Hero = () => {
   return (
-    <div>---hero</div>
+    <div className='ks__haeder section__padding ' id ='home'>
+     <div ks__header-content>
+      <h1 className='gradient__text'>
+      Getting overwhelmed? Get Karmasoft instead!
+      </h1>
+      <p>Multi-functional business management software solution to simplify studio management. From now on, you can focus on what you really love - teaching yoga and fitness.
+          Staff management, sales, booking & scheduling, virtual classes </p>
+      <div className='ks__header-content__input'>
+        <input type='email' placeholder='Start your free trial today!' />
+        <button type='button'>Get Demo</button>
+      </div>
+      <div className='ks__header-image'>
+        <img src={meditating} alt='Boy meditating' />
+      </div>
+     </div>
+    </div>
   )
 }
 
